35d6a1738785a1c1d1d9ddaff0c1cc55b87cf0be
hisomura <1143239+hisomura@users.noreply.github.com>
2016-03-17 08:48:28 +0900

    nyagoにpeco_cd_historyを追加した

--- nyagos_utils.lua
+++ nyagos_utils.lua
@@ -1,26 +1,30 @@
-share.cd = function(arg)
-    r, err = nyagos.exec('__cd__ "' .. arg:gsub('\\', '/') .. '"')
-    share.insert_cd_history(args)
-    if (not share.UpdatePromptAlways) then
-        share.PROMPT = share.makePrompt()
+share.insert_cd_history = function()
+    local cd_history_path = nyagos.getenv("APPDATA") .. '\\NYAOS_ORG\\nyagos.cd_history'
+    f = io.open(cd_history_path, 'a+')
+    f:write(nyagos.getwd() .. '\n')
+    f:flush()
+end
+
+share.cd = function(args)
+    arg1 = ''
+    if #args > 0 then
+        arg1 = args[1]:gsub('\\', '/') 
+        arg1 = '"' .. arg1 .. '"'
     end
+    r, err = nyagos.exec('__cd__ ' .. arg1)
+    share.insert_cd_history()
+
     return r, err
 end
 
-share.insert_cd_history = function(args)
-    nyagos.write('hello')
-end
-    
-
-share.peco_cd = function(this)
-    local history_path = nyagos.getenv("APPDATA") .. '\\NYAOS_ORG\\nyagos.history'
-    local command = nyagos.eval('type ' .. history_path .. ' | peco')
-    if command ~= nil then
-        return command
-    else
-        nyagos.prompt()
+share.peco_cd_history = function(this)
+    local cd_history_path = nyagos.getenv("APPDATA") .. '\\NYAOS_ORG\\nyagos.cd_history'
+    local target_path = nyagos.eval('type ' .. cd_history_path .. ' | peco')
+    if target_path ~= nil then
+        r, err = nyagos.exec('__cd__ ' .. target_path)
     end
-
+    this:call("CLEAR_SCREEN")
+    -- エラーが出たらこの辺りで止めた方がいいかも
     return nil
 end
 
@@ -32,7 +36,7 @@ share.peco_history = function(this)
     return command
 end
 
-share.docker_machine_env = function (args) 
+share.docker_machine_env = function (args)
     if args[1] ~= nil then
         machine = args[1]
     else
