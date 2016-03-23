b44448b2538720724c1e3f0947a719e69bf2984a
hisomura <hisomura@users.noreply.github.com>
2016-03-23 17:25:08 +0900

    実装中

--- nyagos_utils.lua
+++ nyagos_utils.lua
@@ -33,6 +33,35 @@ share.peco_cd_history = function(this)
     return nil
 end
 
+
+-- https://gist.github.com/balaam/3122129
+share.reverse = function (tbl)
+    for i=1, math.floor(#tbl / 2) do
+        tbl[i], tbl[#tbl - i + 1] = tbl[#tbl - i + 1], tbl[i]
+    end
+end
+share.uniq = function (tbl)
+    local uniqtbl
+    for elem in tbl do
+        
+    end
+end
+
+share.peco_history_fix = function(this)
+    local path = nyagos.pathjoin(nyagos.env.appdata, 'NYAOS_ORG\\nyagos.history')
+
+    local commands_str = nyagos.eval('type ' .. path) -- TODO luaの関数に直す
+    commands = {}
+    for command in string.gmatch(commands_str, "([^\n]+)") do
+        commands.insert(command)
+    end
+    commands = share.reverse(commands)
+
+    local result = nyagos.eval('peco < ' .. path)
+    this:call("CLEAR_SCREEN")
+    return result
+end
+
 -- 今後標準で搭載されるので消す予定
 share.peco_history = function(this)
     local path = nyagos.pathjoin(nyagos.env.appdata, 'NYAOS_ORG\\nyagos.history')
