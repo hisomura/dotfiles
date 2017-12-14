fa12c08327821020336a5ec3d4717ca222822d13
hisomura <hisomura@users.noreply.github.com>
2017-12-14 17:59:54 +0900

    WSL用のエイリアスを設定した

---
+++ alias_wsl.sh
@@ -0,0 +1,7 @@
+# vim: ft=sh
+# WSL用設定
+
+alias docker='"/mnt/c/Program Files/Docker/Docker/resources/bin/docker.exe"'
+alias d='"/mnt/c/Program Files/Docker/Docker/resources/bin/docker.exe"'
+alias docker-compose='"/mnt/c/Program Files/Docker/Docker/resources/bin/docker-compose.exe"'
+alias dc='"/mnt/c/Program Files/Docker/Docker/resources/bin/docker-compose.exe"'
