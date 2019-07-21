661ac66552efa10c14e049abc8d2c8b4ded2002b
hisomura <hisomura.noreply@gmail.com>
2019-07-21 13:09:52 +0900

    mklinkの引数間違えてたので修正

--- setup.bat
+++ setup.bat
@@ -1,7 +1,6 @@
 cd
 
-REM vim
-mklink %USERPROFILE%\dotfiles\vimrc %USERPROFILE%\.vimrc
-mklink %USERPROFILE%\dotfiles\gvimrc %USERPROFILE%\.gvimrc
-mklink %USERPROFILE%\dotfiles\ideavimrc %USERPROFILE%\.ideavimrc
-mklink %USERPROFILE%\dotfiles\vsvimrc %USERPROFILE%\.vsvimrc
+mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\vimrc
+mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\gvimrc
+mklink %USERPROFILE%\.ideavimrc %USERPROFILE%\dotfiles\ideavimrc
+mklink %USERPROFILE%\.vsvimrc %USERPROFILE%\dotfiles\vsvimrc
