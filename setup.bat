04a17bba9fc2bf278b2ebdd9e2ca2ce55fb99ec6
hisomura <1143239+hisomura@users.noreply.github.com>
2026-02-07 15:41:10 +0900

    remove gvimrc

--- setup.bat
+++ setup.bat
@@ -1,6 +1,5 @@
 cd
 
 mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\vimrc
-mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\gvimrc
 mklink %USERPROFILE%\.ideavimrc %USERPROFILE%\dotfiles\ideavimrc
 mklink %USERPROFILE%\.vsvimrc %USERPROFILE%\dotfiles\vsvimrc
